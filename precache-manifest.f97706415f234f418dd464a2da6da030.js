5d8993408e62787ddfb6baa1e87a2d6b1e35e424
cedeerwe <cdominik@gmail.com>
2019-11-18 23:54:31 +0100

    Updates

---
+++ precache-manifest.f97706415f234f418dd464a2da6da030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3748d08683129bfdb6a33a985c0bc88d",
+    "url": "/read-my-mind/index.html"
+  },
+  {
+    "revision": "09ddef13f2db779ab5ee",
+    "url": "/read-my-mind/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "5bbcb897d51c16b4dc04",
+    "url": "/read-my-mind/static/css/main.7b52e7e6.chunk.css"
+  },
+  {
+    "revision": "09ddef13f2db779ab5ee",
+    "url": "/read-my-mind/static/js/2.aeeeddab.chunk.js"
+  },
+  {
+    "revision": "5bbcb897d51c16b4dc04",
+    "url": "/read-my-mind/static/js/main.fbeae26b.chunk.js"
+  },
+  {
+    "revision": "e030583d8f257aaa338b",
+    "url": "/read-my-mind/static/js/runtime-main.974690a2.js"
+  }
+]);
